2ca1156e23da3899a3d93c906c3e6110de7e589d
Lilynews <lilinews2021@gmail.com>
2022-12-08 21:46:51 -0500

    commit app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+/////// setting variable, file
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+/////// use public static files
+app.use(express.static('public'))
+
+/////// setting express-handlebars
+app.engine('handlebars', exphbs({ defaultLayout:'main' }))
+app.set('view engine', 'handlebars')
+
+/////// setting routes
+// for restaurant-list 
+app.get('/', (req,res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+
+// for restaurant detail page
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find( restaurant => {
+    return restaurant.id.toString() === req.params.restaurant_id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+/////// setting listening
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
